00fd26554afccfacaeaa357f13763f4370f524e6
chenzhijian <952400152@qq.com>
2018-06-12 22:06:11 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.example.coolweather.util;
+
+/**
+ * Created by 陈志坚 on 2018/6/12.
+ */
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
